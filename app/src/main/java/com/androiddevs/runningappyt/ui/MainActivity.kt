c744db1eb4d9d0ce92c50a3e121b10689df69e21
NguNguyen <>
2023-01-12 14:31:02 -0500

    second commit

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -1,6 +1,10 @@
+@file:Suppress("ktlint:import-ordering")
+
 package com.androiddevs.runningappyt.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +12,10 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
+import com.androiddevs.runningappyt.other.Constants.RADIO_DARK_MODE
+import com.androiddevs.runningappyt.other.Constants.RADIO_LIGHT_MODE
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.navHostFragment
 import kotlinx.android.synthetic.main.activity_main.toolbar
@@ -20,9 +27,28 @@ import kotlinx.android.synthetic.main.activity_main.toolbar
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val lightMode = sharedPreferences.getBoolean(RADIO_LIGHT_MODE, false)
+        val darkMode = sharedPreferences.getBoolean(RADIO_DARK_MODE, false)
+
+        if (darkMode) {
+            setTheme(R.style.DarkTheme)
+        } else if (lightMode) {
+            setTheme(R.style.LightTheme)
+        } else {
+            when (applicationContext.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                Configuration.UI_MODE_NIGHT_YES ->
+                    setTheme(R.style.DarkTheme)
+
+                Configuration.UI_MODE_NIGHT_NO ->
+                    setTheme(R.style.LightTheme)
+            }
+        }
+
         setContentView(R.layout.activity_main)
 
         navigateToTrackingFragmentIfNeeded(intent)
--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/SettingsFragment.kt
@@ -1,3 +1,5 @@
+@file:Suppress("ktlint:import-ordering")
+
 package com.androiddevs.runningappyt.ui.fragments
 
 import android.content.SharedPreferences
@@ -8,6 +10,8 @@ import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.other.Constants
 import com.androiddevs.runningappyt.other.Constants.KEY_NAME
 import com.androiddevs.runningappyt.other.Constants.KEY_WEIGHT
+import com.androiddevs.runningappyt.other.Constants.RADIO_DARK_MODE
+import com.androiddevs.runningappyt.other.Constants.RADIO_DEFAULT_MODE
 import com.androiddevs.runningappyt.other.Constants.RADIO_LIGHT_MODE
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +20,9 @@ import kotlinx.android.synthetic.main.activity_main.tvToolbarTitle
 import kotlinx.android.synthetic.main.fragment_settings.btnApplyChanges
 import kotlinx.android.synthetic.main.fragment_settings.etName
 import kotlinx.android.synthetic.main.fragment_settings.etWeight
+import kotlinx.android.synthetic.main.fragment_settings.radioDarkMode
+import kotlinx.android.synthetic.main.fragment_settings.radioDefaultMode
+import kotlinx.android.synthetic.main.fragment_settings.radioLightMode
 
 /**
  * [Fragment] to manage all user settings.
@@ -44,6 +51,12 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     private fun loadFieldsFromSharedPref() {
         val name = sharedPreferences.getString(KEY_NAME, "")
         val weight = sharedPreferences.getFloat(KEY_WEIGHT, Constants.Default.WEIGHT)
+        val defaultMode = sharedPreferences.getBoolean(RADIO_DEFAULT_MODE, false)
+        val lightMode = sharedPreferences.getBoolean(RADIO_LIGHT_MODE, false)
+        val darkMode = sharedPreferences.getBoolean(RADIO_DARK_MODE, false)
+        radioDefaultMode.isChecked = defaultMode
+        radioLightMode.isChecked = lightMode
+        radioDarkMode.isChecked = darkMode
         etName.setText(name)
         etWeight.setText(weight.toString())
     }
@@ -51,7 +64,9 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     private fun applyChangesToSharedPref(): Boolean {
         val nameText = etName.text.toString()
         val weightText = etWeight.text.toString()
-        val radioLightMode = true
+        val radioDefaultMode = radioDefaultMode.isChecked
+        val radioLightMode = radioLightMode.isChecked
+        val radioDarkMode = radioDarkMode.isChecked
 
         if (nameText.isEmpty() || weightText.isEmpty()) {
             return false
@@ -60,10 +75,13 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         sharedPreferences.edit()
             .putString(KEY_NAME, nameText)
             .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .putBoolean(RADIO_DEFAULT_MODE, radioDefaultMode)
             .putBoolean(RADIO_LIGHT_MODE, radioLightMode)
+            .putBoolean(RADIO_DARK_MODE, radioDarkMode)
             .apply()
         val toolbarText = "Let's go $nameText"
         requireActivity().tvToolbarTitle.text = toolbarText
+        requireActivity().recreate()
         return true
     }
 }
